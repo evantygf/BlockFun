e26daed62cbc3c10c1c8461c612473f5ae3c4fc7
Evan <evan.dunning@gmail.com>
2016-07-01 16:35:49 -0500

    Add copyright notice

--- id_list.py
+++ id_list.py
@@ -1,3 +1,18 @@
+# Copyright 2016 Evan Dunning
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #                image_path,                             name,         id,         type, state, breakable, drops, illuminant
 sky_id =        ("images/tiles/sky.png",                "sky",          0,      "block", 0,     0,          0,      1)
 invisible_id =  ("images/tiles/invisible.png",          "invisible",    1,      "block", 1,     0,          1,      0)
